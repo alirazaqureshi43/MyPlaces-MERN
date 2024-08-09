e2716b4f914d5759ac8fd530d750819ed9313790
Ali Raza <alirazaqureshi43@gmail.com>
2024-08-09 16:48:57 +0500

    cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://my-place-backend-168epwqz4-alirazaqureshi43s-projects.vercel.app',
+        changeOrigin: true,
+      },
+    },
+  },
 })
